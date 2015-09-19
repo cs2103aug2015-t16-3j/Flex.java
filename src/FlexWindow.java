e2ccba56fd2454b77e5a90d7dde4fd30710a8a7b
sohjionghao <sohjnthn@yahoo.com.sg>
2015-09-20 01:31:27 +0800

    git commit

---
+++ src/FlexWindow.java
@@ -0,0 +1,52 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.TextArea;
+
+public class FlexWindow extends JFrame {
+
+	private JPanel contentPane;
+	private TextArea textArea;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FlexWindow frame = new FlexWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FlexWindow() {
+		setTitle("Flex Display");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		textArea = new TextArea();
+		textArea.setEditable(false);
+		contentPane.add(textArea, BorderLayout.CENTER);
+		
+	}
+
+	public TextArea getTextArea(){
+		return textArea;
+	}
+	
+}
