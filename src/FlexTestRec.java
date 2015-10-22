1aacbbc5ed9bea0e647500a94ed4443d601d3ec7
stefangjk <sgjkboy@hotmail.com>
2015-10-22 12:54:08 +0800

    git commit (Test for recurring)

---
+++ src/FlexTestRec.java
@@ -0,0 +1,39 @@
+import static org.junit.Assert.*;
+import java.io.IOException;
+import org.junit.Test;
+
+//FlexTestRec test that an input is a recurring task
+
+//input
+//add <taskname>; <start>-<end> every <day>
+
+//output
+//<taskname>, <start>-<end> every <day>
+
+public class FlexTestRec {
+	
+	@Test
+	public void testOutput() throws IOException {
+		
+		//recurring task input
+		assertTrue(Checker.isRecurringTaskInput("task; 0000-0001 every monday"));
+		assertTrue(Checker.isRecurringTaskInput("task; 0001-0002 every tuesday"));
+		assertTrue(!Checker.isRecurringTaskInput("task; 0002-0003 every wednesday , "));
+		assertTrue(!Checker.isRecurringTaskInput("task; 0003-0004 every thursday; medium"));
+		assertTrue(!Checker.isRecurringTaskInput("task; 0004-0005 every friday bbb"));
+		
+		//boundary cases
+		assertTrue(!Checker.isRecurringTaskInput("task; 00000-2359 every monday"));
+		assertTrue(!Checker.isRecurringTaskInput("task; 0000-2400 every tuesday"));
+		assertTrue(!Checker.isRecurringTaskInput("task; 0000-00000 every wednesday"));
+		
+		//recurring task output
+		assertTrue(Checker.isRecurringTaskOutput("task, 1000-1100 every monday"));
+		assertTrue(Checker.isRecurringTaskOutput("task, 1100-1200 every tuesday"));
+		assertTrue(!Checker.isRecurringTaskOutput("task, 1200-1300 every wednesday ; "));
+		assertTrue(!Checker.isRecurringTaskOutput("task, 1300-1400 every thursday xxx "));
+		assertTrue(!Checker.isRecurringTaskOutput("task, 1400-1500 every friday , "));
+	}
+	
+
+}
