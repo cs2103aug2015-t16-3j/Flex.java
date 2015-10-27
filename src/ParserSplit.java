a2bc47f61cc9afdeeab60c252a373258b68ef309
rib725 <jiujiu1013@gmail.com>
2015-10-27 19:55:06 +0800

    add commandChange method

--- src/ParserSplit.java
+++ src/ParserSplit.java
@@ -2,12 +2,14 @@ import java.util.*;
 public class ParserSplit {
     private static final String COMMAND_ADD = "add";
     private static final String COMMAND_DELETE = "delete";
-    //private static final String COMMAND_CHANGE = "change";
+    private static final String COMMAND_CHANGE = "change";
     private static final String COMMAND_UNDO = "undo";
     //private static final String COMMAND_SHOW = "show";
     //private static final String COMMAND_DISPLAY = "display";
     private static final String COMMAND_SEARCH = "search";
     private static final String COMMAND_EXIT = "exit";
+    
+    
     
     
     public static Command parse(String userInput){
@@ -20,8 +22,8 @@ public class ParserSplit {
 	                         break;
 	      case COMMAND_DELETE: command = deleteCommand(arguments);
 	                           break;
-	      //case COMMAND_CHANGE: command = changeCommand(arguments);
-    	                      // break;
+	      case COMMAND_CHANGE: command = changeCommand(arguments);
+    	                       break;
 	      case COMMAND_UNDO: command = undoCommand();
 	                         break;
 	      //case COMMAND_SHOW: command = showCommand(arguments);
@@ -48,6 +50,7 @@ public class ParserSplit {
     	return temp;
         }
     
+    // add
     private static Command addCommand(String arguments){
     	Command command = new Command(Command.Type.ADD);
     	if(arguments.contains("by")&&arguments.contains("on")){
@@ -97,16 +100,17 @@ public class ParserSplit {
     		return command;
     	}
     	else{
-    		command.setTaskType("floating");
+    		command.setTaskType("Floating");
     		String taskName = arguments.trim();
     		command.setTaskName(taskName);
     		return command;
     	}
     }
+    // delete
     private static Command deleteCommand(String arguments){
     	Command command = new Command(Command.Type.DELETE);
     	if(arguments.contains("floating")){
-    		command.setTaskType("floating");
+    		command.setTaskType("Floating");
     		String[] splitFirst = arguments.split(" ");
     		int number = Integer.valueOf(splitFirst[1]);
     		command.setNumber(number);
@@ -130,22 +134,134 @@ public class ParserSplit {
     	}
     	
     }
-    
+    // change
+    private static Command changeCommand(String arguments) {
+    	Command command = new Command(Command.Type.CHANGE);
+    	if(arguments.contains("rec")){
+    		command.setTaskType("Recurring");
+    		String[] splitRecFirst = arguments.trim().split(" ", 3);
+    		int number = Integer.valueOf(splitRecFirst[1]);
+    		command.setNumber(number);
+    		if(splitRecFirst[2].contains("taskname")){
+    			String[] splitRecSec = splitRecFirst[2].trim().split(" ");
+    			String newRecTaskName = splitRecSec[2].trim();
+    			command.setChangedTaskName(newRecTaskName);
+    		}
+    		else if(splitRecFirst[2].contains("every")){
+    			String[] splitRecThird = splitRecFirst[2].trim().split(" ");
+    			String newDay = splitRecThird[2].trim();
+    			command.setChangedDay(newDay);
+    		}
+    		else if(splitRecFirst[2].contains("time")){
+    			String[] splitRecFourth = splitRecFirst[2].trim().split(" ");
+    			String startEndTime = splitRecFourth[2].trim();
+    			String[] splitRecFifth = startEndTime.split("-");
+    			String newStart = splitRecFifth[0].trim();
+    			String newEnd = splitRecFifth[1].trim();
+    			command.setChangedStartTime(newStart);
+    			command.setChangedEndTime(newEnd);
+    		}
+        }
+    	else if(arguments.contains("floating")){
+      		 command.setTaskType("floating");
+      		 String[] splitFloFirst = arguments.trim().split(" ",3);
+      		 int FloNumber = Integer.valueOf(splitFloFirst[1]);
+      		 command.setNumber(FloNumber);
+      		 if(splitFloFirst[2].contains("to")){
+      			 String[] splitFloSec = splitFloFirst[2].trim().split(" ");
+      			 String newFloTaskName = splitFloSec[1].trim();
+      			 command.setChangedTaskName(newFloTaskName);
+      		 }
+      		 else if(splitFloFirst[2].trim().equals("done")){
+      			 command.setStatus("done");
+      		 }
+      		 else if(splitFloFirst[2].trim().equals("not done")){
+      			 command.setStatus("not done");
+      		 }
+      	}
+    	else if(arguments.contains("by")){
+       		command.setTaskType("Deadline");
+       		String[] splitDeadFirst = arguments.trim().split(" ", 3);
+       		String deadDate = splitDeadFirst[0].trim();
+       		int deadNumber = Integer.valueOf(splitDeadFirst[1]);
+       		command.setDate(deadDate);
+       		command.setNumber(deadNumber);
+       		if(splitDeadFirst[2].contains("end by")){
+       			String[] splitDeadSec = splitDeadFirst[2].trim().split(" ");
+       			String deadEnd = splitDeadSec[2].trim();
+       			command.setChangedEndTime(deadEnd);
+       		}
+       		else if(splitDeadFirst[2].contains("by") && splitDeadFirst[2].contains("on")){
+       			String[] splitDeadThird = splitDeadFirst[2].trim().split(" ");
+       			String newdeadEnd = splitDeadThird[1].trim();
+       			String newdeadDate = splitDeadThird[3].trim();
+       			command.setChangedEndTime(newdeadEnd);
+       			command.setChangedDate(newdeadDate);
+       		}
+       	}
+    	else{
+       		command.setTaskType("Event");
+       		String[] splitFirst = arguments.trim().split(" ", 3);
+       		String date = splitFirst[0];
+       		int number = Integer.valueOf(splitFirst[1]);
+       		command.setDate(date);
+       		command.setNumber(number);
+       		if(splitFirst[2].contains("taskName")){
+       			String[] splitSec = splitFirst[2].trim().split(" ");
+       			String newTaskName = splitSec[2].trim();
+       			command.setChangedTaskName(newTaskName);
+       		}
+       		else if(splitFirst[2].contains("date")){
+       			String[] splitThird = splitFirst[2].trim().split(" ");
+       			String newDate = splitThird[2].trim();
+       			command.setChangedDate(newDate);
+       		}
+       		else if(splitFirst[2].contains("priority")){
+       			String[] splitFourth = splitFirst[2].trim().split(" ");
+       			String newPriority = splitFourth[2].trim();
+       			command.setChangedPriority(newPriority);
+       		}
+       		else if(splitFirst[2].contains("time")){
+       			String[] splitFifth = splitFirst[2].trim().split(" ");
+       			String newTime = splitFifth[2];
+       			String[] newTimeSplit = newTime.trim().split("-");
+       			String newStartTime = newTimeSplit[0];
+       			String newEndTime = newTimeSplit[1];
+       			command.setChangedStartTime(newStartTime);
+       			command.setChangedEndTime(newEndTime);
+       		}
+       		else if(splitFirst[2].trim().equals("not done")){
+       			command.setStatus("not done");
+       		}
+       		else if(splitFirst[2].trim().equals("done")){
+       			command.setStatus("done");
+       		}
+       	}
+    	return command;
+     
+ }
+
+    // search
     private static Command searchCommand(String arguments){
     	Command command = new Command(Command.Type.SEARCH);
     	String keyWord = arguments.trim();
     	command.setKeyWord(keyWord);
     	return command;
     }
+    
+    // undo
     private static Command undoCommand(){
     	Command command = new Command(Command.Type.UNDO);
     	return command;
     }
     
+    // exit 
     private static Command  exitCommand(){
     	Command command = new Command(Command.Type.EXIT);
     	return command;
     }
+    
+    //invalid
     private static Command invalidCommand(){
     	Command command = new Command(Command.Type.INVALID);
     	return command;
