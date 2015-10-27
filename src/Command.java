abf5f8afd22b24fa52da256e481ad9f928f5e3e1
rib725 <jiujiu1013@gmail.com>
2015-10-27 19:53:06 +0800

    Update Command.java

--- src/Command.java
+++ src/Command.java
@@ -13,6 +13,13 @@ public class Command {
      private String priority;
      private int number;
      private String keyWord;
+     private String status;
+     private String changedTaskName;
+     private String changedDate;
+     private String changedStartTime;
+     private String changedEndTime;
+     private String changedDay;
+     private String changedPriority;
      
      public Command(Type type){
     	 this.type = type;
@@ -46,10 +53,31 @@ public class Command {
      public int getNumber(){
     	 return number;
      }
-     
      public String getKeyWord(){
     	 return keyWord;
      }
+     public String getStatus(){
+    	 return status;
+     }
+     public String getChangedTaskName(){
+    	 return changedTaskName;
+     }
+     public String getChangedDate(){
+    	 return changedDate;
+     }
+     public String getChangedStartTime(){
+    	 return changedStartTime;
+     }
+     public String getChangedEndTime(){
+    	 return changedEndTime;
+     }
+     public String getChangedDay(){
+    	 return changedDay;
+     }
+     public String getChangedPriority(){
+    	 return changedPriority;
+     }
+     
      
      public void setTaskType(String taskType){
     	 this.taskType = taskType;
@@ -80,5 +108,25 @@ public class Command {
      public void setKeyWord(String keyWord){
     	 this.keyWord = keyWord;
      }
-     
+     public void setStatus(String status){
+    	 this.status = status;
+     }
+     public void setChangedTaskName(String changedTaskName){
+    	 this.changedTaskName = changedTaskName;
+     }
+     public void setChangedDate(String changedDate){
+    	 this.changedDate = changedDate;
+     }
+     public void setChangedStartTime(String changedStartTime){
+    	 this.changedStartTime = changedStartTime;
+     }
+     public void setChangedEndTime(String changedEndTime){
+    	 this.changedEndTime = changedEndTime;
+     }
+     public void setChangedDay(String changedDay){
+    	 this.changedDay = changedDay;
+     }
+     public void setChangedPriority(String changedPriority){
+    	 this.changedPriority = changedPriority;
+     }
 }
