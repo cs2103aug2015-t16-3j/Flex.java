c8e2f878049d48dc3826949ab62acf1ea8e41ba5
zykzyhzc <jlzc123@hotmail.com>
2015-10-30 14:02:16 +0800

    Update FlexWindow.java

--- src/FlexWindow.java
+++ src/FlexWindow.java
@@ -1,67 +1,121 @@
-import java.awt.EventQueue;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import java.awt.TextArea;
-import javax.swing.JMenuBar;
-import java.awt.GridLayout;
-import java.awt.Color;
 
-public class FlexWindow extends JFrame {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
-	// this is the program output display area
-	private TextArea textArea;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
-	/**
-	 * Launch the application.
-	 */
+public class FlexWindow extends Application {
+	private static TextField input;
+	private static TextArea textArea;
+	private static TextArea feedback;
+	private static String command;
+	private static Stage window;
+	private static String filename;
+	private static boolean haveFilename;
+	private static Button help;
+	private static final String FILENAME_INPUT_MESSAGE = "Please enter the full path name of the .txt schedule file, including its name. For example: C:"
+			+ "\\" + "Users" + "\\" + "Owner" + "\\" + "Documents" + "\\" + "Flex" + "." + "java" + "\\" + "src" + "\\"
+			+ "FlexTest" + "." + "txt";
+	
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					FlexWindow frame = new FlexWindow();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage){
+		window = primaryStage;
+		window.setTitle("Flex");
+
+		input = new TextField();
+		textArea = new TextArea(FILENAME_INPUT_MESSAGE);
+		feedback = new TextArea();
+		command = new String();
+		haveFilename = false;
+		help = new Button("Help");
+		
+		GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(8);
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        Scene scene = new Scene(grid, 800, 600);
+
+        textArea.setPrefColumnCount(40);
+        textArea.setPrefRowCount(20);
+        setWidthAndLength();
+        setEditability();
+        
+        grid.add(help, 0, 0);
+		grid.add(feedback, 0, 4);
+        grid.add(input, 0, 5);
+        grid.add(textArea, 0, 1);
+		input.setPromptText("command");
+		
+		scene.setOnKeyPressed(new EventHandler<KeyEvent>(){
+
+			@Override
+			public void handle(KeyEvent event) {
+				// TODO Auto-generated method stub
+				if(event.getCode().equals(KeyCode.ENTER)){
+					if(haveFilename){
+						textArea.setText("");
+						command = input.getText();
+						filename = FindFilename.getFilename();
+						Flex.processCommand(command, filename);
+					}else{
+						command = input.getText();
+						textArea.setText("");
+						haveFilename = FindFilename.find(command);
+					}
+					input.setText("");
 				}
 			}
+			
 		});
+		
+		window.setOnCloseRequest(new EventHandler<WindowEvent>(){
+
+			@Override
+			public void handle(WindowEvent event) {
+				event.consume();
+				closeProgram();
+			}
+			
+		});
+        
+		window.setScene(scene);
+		window.show();
 	}
-
-	/**
-	 * Create the frame.
-	 */
-	public FlexWindow() {
-		setBackground(new Color(255, 235, 205));
-		setTitle("Flex");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 611, 424);
-
-		// This menu bar contains the read-only labels for the variables shown
-		// in the Task lines
-		JMenuBar menuBar = new JMenuBar();
-		menuBar.setBackground(new Color(220, 220, 220));
-		menuBar.setForeground(new Color(0, 0, 0));
-		setJMenuBar(menuBar);
-
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(new GridLayout(0, 1, 0, 0));
-
-		textArea = new TextArea();
+	
+	public void setEditability(){
 		textArea.setEditable(false);
-		contentPane.add(textArea);
-
+		feedback.setEditable(false);
 	}
-
-	public TextArea getTextArea() {
+	
+	public void setWidthAndLength(){
+		textArea.setPrefHeight(600);
+		input.setPrefWidth(800);
+		feedback.setPrefHeight(20);
+	}
+	
+	public static TextArea getTextArea(){
 		return textArea;
 	}
-
+	
+	private static void closeProgram(){
+		boolean result = ConfirmBox.display();
+		if(result)
+			window.close();
+	}
 }
